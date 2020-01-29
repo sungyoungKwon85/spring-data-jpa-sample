72b7c96a867a22527d823622987fd3d94ce31e44
sungyoungKwon85 <ssyang0@naver.com>
2020-01-29 18:59:38 +0900

    -

--- session/src/main/java/com/kkwonsy/example/session/service/session/EntityRedisRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kkwonsy.example.session.service.session;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface EntityRedisRepository extends CrudRepository<Entity, String> {
-
-}
